28229c8642faf9dffc70ded8e890bc8acc26f676
hwanginkyeong <inke0831@naver.com>
2021-11-18 18:04:27 +0900

    1st

---
+++ src/com/javaex/practice/Ex07.java
@@ -0,0 +1,28 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex07 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요");
+		
+		for(int i=sc.nextInt(); i<10; i++) {
+			for(int num=1; num<=i; num++) {
+				
+				
+			}
+			System.out.println(i);
+		}
+		
+		
+		
+		
+		
+		sc.close();
+		
+	}
+
+}
---
+++ src/com/javaex/practice/Ex08.java
@@ -0,0 +1,15 @@
+package com.javaex.practice;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		
+		for(int dan=2; dan<=9; dan++ ) {
+			for(int i=1; i<10; i++) {
+				System.out.println(dan + "*" + i + "="+dan*i);
+			}
+			System.out.println("");
+		}
+	}
+
+}
